a58b6f80d4ff7744bd2ecfa3b692669b81a156b1
chalos67 <chalosampini@hotmail.com>
2026-05-08 19:37:05 -0300

    Completa CRUD de gestor de tareas

--- gestor.py
+++ gestor.py
@@ -1,17 +1,24 @@
+import os
+
+def limpiar_pantalla():
+       os.system("clear")
+
 def mostrar_menu():
-   
+       
         print("\n === GESTOR DE TAREAS === \n")
         print (" \n === ELIGA UNA OPCION === \n ")
         print ("1- AGREGAR TAREA")
         print ("2- VER TAREA")
-        print ("3- SALIR")
-        
+        print ("3- COMPLETAR TAREA")
+        print ("4- ELIMINAR TAREA")
+        print ("5- SALIR")
+       
 
 def pedir_opcion():
        while True:
             opcion = input("elegir una opcion")
 
-            if opcion in ["1","2","3"]:
+            if opcion in ["1","2","3","4","5"]:
                 return int (opcion)
                   
             print("Opcion incorrecta volver a ingresar")
@@ -19,7 +26,9 @@ def pedir_opcion():
 def agregar_tarea(tareas):
         nueva_tarea = definir_tarea(tareas)
         tareas.append(nueva_tarea)
+        print("\n TAREA AGREGADA CON EXITO \n")
         return(tareas)
+
 
 def definir_tarea(tarea):
       titulo = input ("ingresar titulo de la tarea ")
@@ -33,28 +42,107 @@ def definir_tarea(tarea):
       return(tarea)
 
 def ver_tareas(tareas):
+        
         print ("\n === LISTA DE TAREAS === \n")
-     
+        print("-" * 50)
+        if not tareas:
+                print ("No hay tareas registradas")
+                return
+        
         for tarea in tareas:
-                print(f"Numero : {tarea['id']}")
-                print(f"Titulo : {tarea['titulo']}")
-                print(f"Estado : {tarea['estado']}")
-                print()
+                 print(
+                    f"{tarea['id']} |"
+                    f" {tarea['titulo']} |"
+                    f" {tarea['estado']} |"
+                )
+        print("-" * 50)
+
+def buscar_tarea(tareas , id_tarea):
+       for tarea in tareas:
+              if tarea["id"] == id_tarea:
+                return tarea
+       
+                      
+
+def elegir_tarea(tareas):
+        print("\n  === Elegir el numero de tarea a seleccionar === \n")
+        ver_tareas()
+        numero = int (input ())
+        tarea = buscar_tarea(tareas,numero)
+        return tarea
+        
+
+def eliminar_tarea(tareas):
+       ver_tareas (tareas)
+       numero = int (input("eliga el numero de tarea a eliminar"))
+       tarea = buscar_tarea(tareas,numero)
+       
+       if tarea is None:
                 
+                print("Tarea no encontrada")
+                return
+
+       
+       
+       print ("\n === TAREA SELECCIONADA === \n")
+       print(f"Numero : {tarea['id']}")
+       print(f"Titulo : {tarea['titulo']}")
+       print(f"Estado : {tarea['estado']}")
+       confirmacion = input ("\n Estas seguro que quieres eliminar tarea 1-si 2-no \n")
+
+       if confirmacion == "1":
+              tareas.remove(tarea)
+              print ("TAREA ELIMINADA")
+       else:
+              print ("ELIMINACION CANCELADA")
+
+def completar_tarea(tareas):
+       ver_tareas (tareas)
+       numero = int (input("eliga el numero de tarea a completar"))
+       tarea = buscar_tarea(tareas,numero)
+       
+       if tarea is None:
+                
+                print("Tarea no encontrada")
+                return
+       
+       print ("\n === TAREA SELECCIONADA === \n")
+       print(f"Numero : {tarea['id']}")
+       print(f"Titulo : {tarea['titulo']}")
+       print(f"Estado : {tarea['estado']}")
+       confirmacion = input ("\n Estas seguro que quieres marcar como completada tarea 1-si 2-no \n")
+
+       if confirmacion == "1":
+              tarea["estado"] = "completado"
+              print ("TAREA COMPLETADA")
+       else:
+              print ("ACCION CANCELADA")
+       
+       
+       
 def main ():
     
         tareas = []
+        
         while True:
+                limpiar_pantalla()
                 mostrar_menu()
                 opcion = pedir_opcion()
                 if opcion == 1:
                         agregar_tarea(tareas)
-                if opcion == 2:
-                        ver_tareas(tareas) 
-                if opcion == 3:
+                        input("Ingrese enter para continuar")
+                elif opcion == 2:
+                        ver_tareas(tareas)
+                        input("Ingrese enter para continuar")
+                elif opcion == 3:
+                        completar_tarea(tareas) 
+                        input("Ingrese enter para continuar")
+                elif opcion == 4:
+                       eliminar_tarea(tareas)
+                       input("Ingrese enter para continuar")
+                elif opcion == 5:
                         print("Saliendo del programa...")
                         break     
-       
 
 if __name__ == "__main__":
     main()
