20b2aaf09c1fe6288bcb3c5d0fe81663fb5dc7a0
chalos67 <chalosampini@hotmail.com>
2026-05-08 17:58:28 -0300

    Se completaron a revisar agregar y ver tareas

--- gestor.py
+++ gestor.py
@@ -12,16 +12,48 @@ def pedir_opcion():
             opcion = input("elegir una opcion")
 
             if opcion in ["1","2","3"]:
-                return opcion
+                return int (opcion)
                   
             print("Opcion incorrecta volver a ingresar")
 
+def agregar_tarea(tareas):
+        nueva_tarea = definir_tarea(tareas)
+        tareas.append(nueva_tarea)
+        return(tareas)
 
+def definir_tarea(tarea):
+      titulo = input ("ingresar titulo de la tarea ")
+
+      tarea = {
+           "id" : len(tarea) + 1,
+           "titulo" : titulo,
+           "estado" : "pendiente"
+          
+      }
+      return(tarea)
+
+def ver_tareas(tareas):
+        print ("\n === LISTA DE TAREAS === \n")
+     
+        for tarea in tareas:
+                print(f"Numero : {tarea['id']}")
+                print(f"Titulo : {tarea['titulo']}")
+                print(f"Estado : {tarea['estado']}")
+                print()
+                
 def main ():
     
-       
-        mostrar_menu()
-        opcion = pedir_opcion()
+        tareas = []
+        while True:
+                mostrar_menu()
+                opcion = pedir_opcion()
+                if opcion == 1:
+                        agregar_tarea(tareas)
+                if opcion == 2:
+                        ver_tareas(tareas) 
+                if opcion == 3:
+                        print("Saliendo del programa...")
+                        break     
        
 
 if __name__ == "__main__":
